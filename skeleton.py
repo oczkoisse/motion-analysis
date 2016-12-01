740db8185752cb8f429c3695bd6d53b83bb15ef1
oczkoisse <oczkoisse@pm.me>
2016-12-01 11:37:59 -0700

    Changed interface to skeleton class

--- skeleton.py
+++ skeleton.py
@@ -5,16 +5,14 @@ class Skeleton():
     def __init__(self, file_name):
         self.file_name = file_name
 
-    # Pulls up torso skeleton data from a general skeleton file, and normalizes it, if given a valid norm value
+    # Pulls up torso skeleton data from a general skeleton file
 
-    def load(self, norm='none'):
+    def load(self):
         self.data = np.loadtxt(self.filename, dtype='float', delimiter=',', skiprows=1, usecols=(9, 10, 11, 18, 19, 20, 27, 28, 29, 36, 37, 38, 45, 46, 47, 54, 55, 56, 63, 64, 65, 72, 73, 74, 81, 82, 83, 90, 91, 92, 99, 100, 101, 108, 109, 110, 189, 190, 191))
 
-        self.normalize(norm)
+    # Normalizes the data, if given a valid norm value. Valid norm values are 'spine-base'
 
-    # Normalizes the data
-
-    def __normalize(self, norm):
+    def normalize(self, norm):
         if norm == 'spine-base':
 
             # Normalizes the data around the mean of spinal base point
