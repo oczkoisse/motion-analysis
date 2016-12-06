f2816065b2ab75c9c4ef667334d0a5e922e1c16a
oczkoisse <oczkoisse@pm.me>
2016-12-05 21:31:38 -0700

    Added comments, corrected the condition for sequence check

--- other/sk_x.py
+++ other/sk_x.py
@@ -5,6 +5,7 @@ gestures = ['RA: move, up', 'RA: move, down', 'LA: move, up', 'LA: move, down', 
             
 with open('Labels.tsv') as labels:
     csvreader = csv.reader(labels, delimiter='\t')
+    # To keep track of a possible pair of gestures
     last_row = []
     for row in csvreader:
         file = row[2]
@@ -13,6 +14,7 @@ with open('Labels.tsv') as labels:
             continue
         start, end, label = row[8:11]
         label = label.strip(';')
+        # Individual labels, note that there won't be any ;
         indi_labels = label.split('; ')
 
         # If we don't have the last row, then there is no transition to clip, so go to next row
@@ -21,13 +23,14 @@ with open('Labels.tsv') as labels:
             continue
         else:
             # If we have some valid gesture in current frame, then ok
-            seq = False
+            # note that validity is defined as two frames having at least a gesture each from the set of valid gestures
+            valid_pair = False
             for i in indi_labels:
                 if i in gestures:
-                    seq = True
+                    valid_pair = True
                     break
-            # If the sequence of gestures is alright, and the current frame follows the previous one
-            if seq and start > last_row[9]: # last_row[9] is the end time in previous row
+            # Ensure that the pair is valid and the current frame follows the previous one
+            if valid_pair and start >= last_row[9]: # last_row[9] is the end time in previous row
                 # Clip the appropriate lines in the corresponding skeleton file
                 file_path_components = file.split('\\')
                 path_to_sk_file = Path('..') / 'z' / file_path_components[0] / file_path_components[1] / file_path_components[2][:-4] + '.txt'
